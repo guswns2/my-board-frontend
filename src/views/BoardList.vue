f9bb4c64190ddc5c104fc9208a617d19a7b15e9e
guswns2 <wkdguswns6@gmail.com>
2024-12-10 23:44:02 +0900

    vuetify header menu 추가중

--- src/views/BoardList.vue
+++ src/views/BoardList.vue
@@ -1,9 +1,17 @@
 <script setup lang="ts">
+const fns = {
+  clickBtn() {
+    console.log("clickBtn");
+  }
+}
 </script>
 
 <template>
   <div>
     <h4>BoardList</h4>
+    <v-btn @click="fns.clickBtn">
+      Button
+    </v-btn>
   </div>
 </template>
 
