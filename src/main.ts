7e4307cf75253c2421b4e2bee617ce0e10eeb8ce
wkdguswns6 <wkdguswns6@gmail.com>
2025-02-02 19:15:23 +0900

    Home 검색필터

--- src/main.ts
+++ src/main.ts
@@ -4,14 +4,18 @@ import App from './App.vue'
 import { router } from "./router";
 
 // Vuetify
-// import 'vuetify/styles'
+import 'vuetify/lib/styles/main.css'
 import { createVuetify } from 'vuetify'
 import * as components from 'vuetify/components'
 import * as directives from 'vuetify/directives'
+import '@mdi/font/css/materialdesignicons.css'
 
 const vuetify = createVuetify({
     components,
-    directives
+    directives,
+    icons: {
+        defaultSet: 'mdi'
+    }
 })
 
 createApp(App).use(router).use(vuetify).mount('#app')
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -4,14 +4,17 @@ import type { VDataTable } from "vuetify/components/VDataTable";
 
 const data = {
   table: reactive({
-    search: '',
+    searchHeader: '제목',
+    searchWord: '',
     headers: [
       { key: 'title', title: '제목', align: 'center', sortable: true },
       { key: 'hashtag', title: '해시태그', align: 'center', sortable: false },
       { key: 'name', title: '작성자', align: 'center', sortable: true },
       { key: 'date', title: '작성일', align: 'center', sortable: true },
     ] as VDataTable["$props"]["headers"],
-    items: []
+    items: [
+      { title: "제목1", hashtag: "해시태그1", name: "이름1", date: "오늘"}
+    ]
   })
 }
 
@@ -23,46 +26,39 @@ const data = {
   <div class="g-view-container">
     <div class="g-view-main">
       <v-card class="g-content-card">
-        <v-row style="width: 100%">
-          <v-col cols="2">
-            <v-autocomplete
-                variant="outlined"
-                hide-details
-                :items="data.table.headers"
-                item-title="title"
-                item-value="key"
-            >
-            </v-autocomplete>
-          </v-col>
-          <v-col cols="8  ">
-            <v-text-field
-                v-model="data.table.search"
-                label="검색어"
-                prepend-inner-icon="mdi-magnify"
-                variant="outlined"
-                hide-details
-                single-line
-            ></v-text-field>
-          </v-col>
-          <v-col cols="1">
-            <v-btn prepend-icon="mdi-magnify">조회</v-btn>
-          </v-col>
-        </v-row>
-
         <v-data-table
           :headers="data.table.headers"
           :items="data.table.items"
-          :search="data.table.search"
+          :search="data.table.searchWord"
         >
           <template #top>
-            <v-autocomplete
-                variant="outlined"
-                :hide-details="true"
-                :items="data.table.headers"
-                item-title="title"
-                item-value="key"
-            >
-            </v-autocomplete>
+            <v-row class="search-row" style="width: 100%;">
+              <v-col cols="2">
+                <v-autocomplete
+                    v-model="data.table.searchHeader"
+                    label="검색항목"
+                    variant="outlined"
+                    hide-details
+                    :items="data.table.headers"
+                    item-title="title"
+                    item-value="key"
+                >
+                </v-autocomplete>
+              </v-col>
+              <v-col cols="8">
+                <v-text-field
+                    v-model="data.table.searchWord"
+                    label="검색어"
+                    variant="outlined"
+                    hide-details
+                    single-line
+                >
+                </v-text-field>
+              </v-col>
+              <v-col cols="2">
+                <v-btn prepend-icon="mdi-magnify">조회</v-btn>
+              </v-col>
+            </v-row>
           </template>
         </v-data-table>
       </v-card>
@@ -71,21 +67,12 @@ const data = {
 </template>
 
 <style scoped>
-.v-data-table {
-  border: 1px solid #000000;
+/* common */
+.search-row {
+  height: fit-content;
+  align-items: center;
 }
-:deep(.v-data-table-footer) {
-  justify-content: center;
-}
-
-.v-row > .v-col {
-  padding-top: 0;
-  padding-bottom: 0;
-}
-.v-row > .v-col:nth-child(1) {
-  padding-left: 0;
-}
-.v-row > .v-col:nth-last-child(1) {
-  padding-right: 0;
+.search-row > .v-col {
+  height: fit-content;
 }
 </style>
